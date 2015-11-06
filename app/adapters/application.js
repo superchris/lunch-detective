02d45a250ea3b9ba4fac8a39efe1374ec389ee43
Chris Nelson <superchrisnelson@gmail.com>
2015-11-06 17:20:37 -0500

    point to real app

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: "/api",
+  host: "http://lunch-detective-server.herokuapp.com",
   pathForType: function(type) {
     return Ember.String.pluralize(Ember.String.underscore(type));
   }
